4ca22f8bf0a7edfd2a03f877e1c59dccb8498f5a
Arthur Pereira <dogfaminto7777@gmail.com>
2022-11-28 21:46:20 -0300

    commit inicial

---
+++ ProjetoFinal_API/Data/ReceitaContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoFinal_API.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ProjetoFinal_API.Data {
+
+    public class ReceitaContext : DbContext {
+        protected readonly IConfiguration Configuration;
+        public ReceitaContext(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("StringConexaoSQLServer"));
+        }
+
+        public DbSet<Receita>? Receita { get; set; }
+    }
+}
